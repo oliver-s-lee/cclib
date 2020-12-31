c4f908b0a64f429c5485f59fa5b936b32cd8dff3
oliver-s-lee <osl@st-andrews.ac.uk>
2020-12-31 16:26:46 +0000

    Expanded test suite for Turbomole

--- test/data/testSP.py
+++ test/data/testSP.py
@@ -132,7 +132,6 @@ class GenericSPTest(unittest.TestCase):
     @skipForParser('Molcas','The parser is still being developed so we skip this test')
     @skipForParser('Molpro', '?')
     @skipForParser('ORCA', 'ORCA has no support for symmetry yet')
-    @skipForParser('Turbomole','The parser is still being developed so we skip this test')
     def testsymlabels(self):
         """Are all the symmetry labels either Ag/u or Bg/u?"""
         sumwronglabels = sum([x not in ['Ag', 'Bu', 'Au', 'Bg'] for x in self.data.mosyms[0]])
@@ -176,6 +175,7 @@ class GenericSPTest(unittest.TestCase):
 
     @skipForParser('DALTON', 'mocoeffs not implemented yet')
     @skipForLogfile('Jaguar/basicJaguar7', 'Data file does not contain enough information. Can we make a new one?')
+    @skipForParser('Turbomole', 'Use of symmetry has reduced the number of mo coeffs')
     def testdimmocoeffs(self):
         """Are the dimensions of mocoeffs equal to 1 x nmo x nbasis?"""
         if hasattr(self.data, "mocoeffs"):
@@ -294,7 +294,6 @@ class GenericSPTest(unittest.TestCase):
     @skipForParser('Molcas', 'reading basis set names is not implemented')
     @skipForParser('ORCA', 'reading basis set names is not implemented')
     @skipForParser('Psi4', 'reading basis set names is not implemented')
-    @skipForParser('Turbomole', 'reading basis set names is not implemented')
     def testmetadata_basis_set(self):
         """Does metadata have expected keys and values?"""
         self.assertEqual(self.data.metadata["basis_set"].lower(), "sto-3g")
@@ -426,6 +425,12 @@ class TurbomoleSPTest(GenericSPTest):
     """Customized restricted single point unittest"""
 
     num_scf_criteria = 2
+    
+    def testmetadata_basis_set(self):
+        """Does metadata have expected keys and values?"""
+        # One of our test cases used sto-3g hondo
+        valid_basis = self.data.metadata["basis_set"].lower() == "sto-3g" or self.data.metadata["basis_set"].lower() == "sto-3g hondo"
+        self.assertTrue(valid_basis)
 
 
 class GenericDispersionTest(unittest.TestCase):
